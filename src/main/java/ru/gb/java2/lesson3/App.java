ef3a3ee9aaf58f0395171c6954b66a8f935ab886
DenisAnanin <denis-anapa@yandex.ru>
2022-03-31 00:17:10 +0300

    Начало домашнего задания Java2 Урок 3

--- src/main/java/ru/gb/java2/lesson3/App.java
+++ src/main/java/ru/gb/java2/lesson3/App.java
@@ -1,4 +1,32 @@
 package ru.gb.java2.lesson3;
 
+import java.util.*;
+
 public class App {
+    public static void main(String[] args) {
+        counting();
+    }
+
+    public static void counting() {
+        ArrayList<String> words = new ArrayList<>();
+        words.add("Три");
+        words.add("Три");
+        words.add("Один");
+        words.add("Один");
+        words.add("Три");
+        words.add("Пять");
+        words.add("Два");
+        words.add("Пять");
+
+        HashSet<String> temp = new HashSet<>(words);
+        for (String s : temp) {
+            int t = 0;
+            for (String s1 : words) {
+                if(s.equals(s1)){
+                t++;
+                }
+            }
+            System.out.println("Слово " + s + " повторяется " + t);
+        }
+    }
 }
